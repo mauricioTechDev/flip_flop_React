50ea3d32a8640add3b05f7d108f17e601fac6996
Mauricio Acosta <mauricio.acosta92@gmail.com>
2020-05-18 01:45:23 -0400

    added css to the confirmation page

--- client/src/components/Confirmation.js
+++ client/src/components/Confirmation.js
@@ -37,7 +37,7 @@ const Confirmation = ({ setAuth }) => {
       });
 
       const parseData = await res.json();
-      setThankYou('Thanks for confirming.  You can log in and enjoy 🚀')
+      setThankYou('Thanks for confirming.  You can log in bellow and enjoy 🚀')
 
       console.log('data for confimring email',parseData);
     } catch (err) {
@@ -45,19 +45,90 @@ const Confirmation = ({ setAuth }) => {
     }
   };
   console.log('thank you', thankYou);
-
+  const changeBackground = (e) => {
+    e.target.style.background = 'orange'
+  }
+  const h1Out = (e) => {
+    e.target.style.background = ''
+  }
+  const changeButtonBackground = (e) => {
+    e.target.style.background = '#00c2ff'
+  }
+  const buttonOut = (e) => {
+    e.target.style.background = ''
+  }
 
   return (
-    <div className="jumbotron mt-5">
-      <h1>FLIP FLOP PHOTO APP</h1>
-      <p>Click bellow to Confirm Your Email</p>
-      <p>{ thankYou }</p>
-      <button onClick={emailConfirmation}>CONFIRM</button>
-      <Link to="/login" className="btn btn-primary">
-        Login
-      </Link>
+    <div style={parentContainer}>
+    <div style={containerLeft}>
+      <h1 style={h1} onMouseEnter={changeBackground} onMouseLeave={h1Out}>FLIP FLOP PHOTO APP</h1>
+      <h2 style={h2} onMouseEnter={changeBackground} onMouseLeave={h1Out}>
+        Click bellow to Confirm Your Email
+      </h2>
+      <h2 style={h2} onMouseEnter={changeBackground} onMouseLeave={h1Out}>{ thankYou }</h2>
+      <button onClick={emailConfirmation} className="btn btn-success btn-block" style={button}>CONFIRM</button>
+    </div>
+    <div style={containerCenter}>
+    <Link className="btn btn-warning btn-lg mt-5" to='/login' style={buttons}>Log In</Link>
+
+    </div>
     </div>
   );
 };
+const parentContainer = {
+  display: 'flex',
+  flexDirection: 'column',
+  flexWrap: 'nowrap',
+  border: '1px solid purple',
+  justifyContent: 'center',
+  backgroundColor: '#fbcbd4'
+};
+const h1 = {
+  marginTop: '25px',
+  fontSize: '3rem',
+  textAlign: 'center',
+  fontFamily: '-webkit-pictograph',
+  marginLeft: '15%',
+  marginRight: '16%',
+  borderRadius: '4%'
+};
+const h2 = {
+  marginTop: '25px',
+  fontSize: '2rem',
+  textAlign: 'center',
+  marginLeft: '15%',
+  marginRight: '16%',
+  fontFamily: '-webkit-pictograph',
+  borderRadius: '4%'
+};
+const containerLeft ={
+  backgroundImage: `url(${'/images/girl-friends-laughing.jpg'})`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  fontWeight: 'bolder',
+  color: 'white',
+  paddingTop: '10%',
+  paddingLeft: '15%',
+  paddingRight: '15%',
+  paddingBottom: '10%',
+  margin: '3%',
+  marginTop: '22%',
+  border: '1px solid purple',
+  boxShadow: '2px 3px 3px 9px #80808073'
+};
+const containerCenter = {
+  textAlign: 'center'
+}
+const button = {
+  width: '40%',
+  marginLeft: '30%'
+}
+const buttons = {
+  border: '3px solid black',
+  boxShadow: 'rgba(128, 128, 128, 0.45) 3px 3px 7px 2px',
+  marginBottom: '20%'
+};
+
 
 export default Confirmation;
